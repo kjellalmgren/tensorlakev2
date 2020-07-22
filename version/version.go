8c9bd55848a87db8109e1ba94c1eb62e43895d77
kjellalmgren <kjell.almgren@tetracon.se>
2020-07-22 23:50:06 +0200

    Minor spelling erros has been fixed

--- version/version.go
+++ version/version.go
@@ -4,15 +4,15 @@ package version
 
 // ServerVersion - version number (exported)
 func ServerVersion() string {
-	return "v0.1.6"
+	return "v0.1.8"
 }
 
 // ModelVersion (Open API version)
 func ModelVersion() string {
-	return "v0.1.6"
+	return "v0.1.8"
 }
 
 // ModelDate change date
 func ModelDate() string {
-	return "2020-05-23"
+	return "2020-07-22"
 }
