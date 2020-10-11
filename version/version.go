2db5eab3b0f8d39fae174800c5bcee1ab8d1c496
kjellalmgren <kjell.almgren@tetracon.se>
2020-10-11 21:26:40 +0200

    Minor changes

--- version/version.go
+++ version/version.go
@@ -4,15 +4,15 @@ package version
 
 // ServerVersion - version number (exported)
 func ServerVersion() string {
-	return "v0.1.8"
+	return "v0.1.9"
 }
 
 // ModelVersion (Open API version)
 func ModelVersion() string {
-	return "v0.1.8"
+	return "v0.1.9"
 }
 
 // ModelDate change date
 func ModelDate() string {
-	return "2020-07-22"
+	return "2020-10-11"
 }
